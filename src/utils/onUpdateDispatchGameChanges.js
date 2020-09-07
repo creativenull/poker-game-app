cdd5eef3085beffbb6b80a3d84752cb95a783290
Arnold Chand <chand.arnold@hotmail.com>
2020-09-07 14:09:02 -0400

    feat: add logger feature when game ends

--- src/utils/onUpdateDispatchGameChanges.js
+++ src/utils/onUpdateDispatchGameChanges.js
@@ -8,28 +8,24 @@ import {
   gameUpdateTotalCredits
 } from '#store/game/actions'
 import { GameState } from '#app/constant-types'
+import logger from '#config/logger'
 
 export default function onUpdateDispatchGameChanges (gameState, hideDealer) {
   if (gameState === GameState.INIT) {
     store.dispatch(gameGetAllHandsAction())
-    // TODO:
-    // Start tracking the game session here
   } else if (gameState === GameState.START) {
     store.dispatch(resetBetCredits())
     store.dispatch(updateDealerView())
     store.dispatch(gameResetPokerAction())
     store.dispatch(gameGetAllHandsAction())
-    // TODO:
-    // Tracking game session
-  } else if (gameState === GameState.CONTINUE) {
-    // TODO:
-    // Tracking game session
   } else if (gameState === GameState.END) {
     store.dispatch(updateDealerView({ hidden: false }))
     store.dispatch(gameGetWinnerAction())
     store.dispatch(gameUpdateTotalCredits())
+
     // TODO:
     // Tracking game session
+    logger.add()
   } else {
     if (!hideDealer) {
       store.dispatch(updateDealerView())
