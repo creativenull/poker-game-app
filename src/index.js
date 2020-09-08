f6b6b0ac0f40f7e570ce94d17c8469cfae1c8ad6
Arnold Chand <chand.arnold@hotmail.com>
2020-09-08 09:32:10 -0400

    feat: add default log header if does not exist

--- src/index.js
+++ src/index.js
@@ -6,10 +6,15 @@ import { Provider } from 'react-redux'
 import store from './store'
 
 import { isSettings, setSettings, defaultSettings } from '#config/settings'
+import logger from '#config/logger'
 
 document.addEventListener('DOMContentLoaded', () => {
   if (!isSettings()) {
     setSettings(defaultSettings)
+  }
+
+  if (!logger.exists()) {
+    logger.setDefault()
   }
 
   ReactDOM.render(
